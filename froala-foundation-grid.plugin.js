775b469a3a854f3363ee5c43e9b70880037bf3d1
rwade <rwade@grayloon.com>
2017-05-31 13:27:37 -0500

    initial files

---
+++ froala-foundation-grid.plugin.js
@@ -0,0 +1,100 @@
+/**
+ * Created by Robert Wade on 9/2/16.
+ */
+// Define an icon.
+$.FroalaEditor.DefineIcon('foundationGridIcon', { NAME: 'th-large'})
+
+// Define a dropdown button.
+$.FroalaEditor.RegisterCommand('foundationGridRowDropdown', {
+    // Button title.
+    title: 'Foundation Grid',
+
+    // Mark the button as a dropdown.
+    type: 'dropdown',
+
+    // Specify the icon for the button.
+    // If this option is not specified, the button name will be used.
+    icon: 'foundationGridIcon',
+
+    // Options for the dropdown.
+    options: {
+        "12x1" : "1|1|1|1|1|1|1|1|1|1|1|1",
+        "2x6" : "2|2|2|2|2|2",
+        "3x4" : "3|3|3|3",
+        "4x3" : "4|4|4",
+        "6x2" : "6|6",
+        "7|5" : "7|5",
+        "5|7" : "5|7",
+    },
+
+    // If present, the options property will be ignored.
+    // It can be used to define a custom HTML for the dropdown.
+    //html: function () {
+        // The current context is the editor instance.
+    //    return '';
+    //},
+
+    // Save the dropdown action into undo stack.
+    undo: true,
+
+    // Focus inside the editor before callback.
+    focus: true,
+
+    // Refresh the button state after the callback.
+    refreshAfterCallback: true,
+
+    // Callback.
+    callback: function (cmd, val, params) {
+        // The current context is the editor instance.
+        //this.html.insertImageHTML("<img src='" + val + '" />');
+
+        var $grid = "<div class='row'>";
+        switch ($val) {
+            case "12x1":
+                $grid += this.repeatColumn(1,12);
+                break;
+            default:
+            case "2x6":
+                $grid += this.repeatColumn(2,6);
+                break;
+            case "3x4":
+                $grid += this.repeatColumn(3,4);
+                break;
+            case "4x3":
+                $grid += this.repeatColumn(4,3);
+                break;
+            case "6x2":
+                $grid += this.repeatColumn(6,2);
+                break;
+            case "7|5":
+                $grid += "<div class='columns medium-7'></div><div class='columns medium-5'></div>";
+                break;
+            case "5|7":
+                $grid += "<div class='columns medium-5'></div><div class='columns medium-7'></div>";
+                break;           }
+
+        this.html.insert("<img src='"+ val + "' />");
+        this.undo.saveStep();
+    },
+
+    repeatColumn: function($col,$inc) {
+        var $columns = "";
+        for ($i = 0; $i < $inc; $i++) {
+            $columns += "<div class='columns medium-"+$col+"'></div>";
+        }
+
+        return $columns;
+    },
+
+    // Called when the dropdown button state might have changed.
+    refresh: function ($btn) {
+        // The current context is the editor instance.
+        //console.log ($btn.selection);
+    },
+
+    // Called when the dropdown is shown.
+    refreshOnShow: function ($btn, $dropdown) {
+        // The current context is the editor instance.
+        //console.log (this.selection.element());
+    }
+});
